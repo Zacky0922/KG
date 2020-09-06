94cdfb14b2e3145d846c1b087586301b148f3ac4
Zacky <abcn00200@yahoo.co.jp>
2020-09-07 01:00:51 +0900

    KG/index.html ESmodule
    ESmoduleとbootstrapの導入

---
+++ importer.js
@@ -0,0 +1,6 @@
+export {
+	zTab, getGicon,
+	zDate, zList, burgerMenu, zMathJax, zMath, zDebug
+}
+	//from "../Zacky0922.github.io/exScripts/aggregater.js";	// for online
+	from "../exScripts/aggregater.js";	// for online
